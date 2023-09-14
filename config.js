f527f1a3941e21f362e33be38cbce7c01e01e0b2
Phạm Quang Hoàn <129405576+gatapbay@users.noreply.github.com>
2023-09-14 15:43:39 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+module.exports = Object.freeze({
+    // DATABASE
+    MONGODB_CONNECT: 'mongodb+srv://quanghoanr:Matkhau01@quyet-dau.oehglzc.mongodb.net/quyet-dau?retryWrites=true&w=majority',
+
+    // JWT Setup
+    JWT_SECURITY_KEY: 'gatapbay',
+    JWT_TOKEN_EXPIRES: '30d',
+
+    // PERMISSION 1 - 5
+    USERS_MANAGER_LEVEL: 4,
+    PRODUCTS_MANAGER_LEVEL: 3,
+    SUPPORT_LEVEL: 2,
+    ORDER_MANAGER_LEVEL: 3,
+
+    // NODE MAILER
+    ORDERS_CONFIRM_MAIL: false,
+    NODEMAILER_HOST: 'smtp.gmail.com',
+    NODEMAILER_PORT: 465,
+    NODEMAILER_USER: 'hoanpqFX22279@funix.edu.vn',
+    NODEMAILER_PASS: '86xmd88RjPgh'
+});
