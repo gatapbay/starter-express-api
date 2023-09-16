fc2818a88fa493c021587495302558b89c459758
Phạm Quang Hoàn <129405576+gatapbay@users.noreply.github.com>
2023-09-16 18:44:55 +0700

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 // Một số thiết lập cho server
 app.disable('etag');
-app.use(cors());
+app.use(cors({origin: '*'}));
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
